f0f35035692f049cd230b1c7a847305de6bfe650
Amit Gupta <amit_rashi@hotmail.com>
2024-11-10 14:25:32 +0000

    Fixing Account ID Issue

--- providers.tf
+++ providers.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "eu-west-2"
   assume_role {
-    role_arn     = "arn:aws:iam::/${var.account_id}:role/${var.session_role_name}"
+    role_arn     = "arn:aws:iam::${var.account_id}:role/${var.session_role_name}"
     session_name = "terraform-mkalehi"
   }
 }
